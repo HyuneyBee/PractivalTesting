9df18b0278da67626b08e5af886ab37c19e21aa9
HyuneyBee <gsh2448@gmail.com>
2024-01-18 23:35:26 +0900

    Persistence Layer 테스트(1)

---
+++ src/main/java/sample/cafekiosk/spring/domain/product/Product.java
@@ -0,0 +1,27 @@
+package sample.cafekiosk.spring.domain.product;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String productNumber;
+
+    @Enumerated(EnumType.STRING)
+    private ProductType type;
+
+    @Enumerated(EnumType.STRING)
+    private ProductSellingType productSellingType;
+
+    private String name;
+
+    private int price;
+}
